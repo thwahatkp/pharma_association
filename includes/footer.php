e101ed9158e319e9936121829126b0cddd35ac7e
thwahatkp <thwahacodeorganization@gmail.com>
2022-11-14 14:34:07 +0530

    modified home page and about page

---
+++ includes/footer.php
@@ -0,0 +1,94 @@
+<footer class="site-footer">
+      <div class="footer-top" style="
+            background-image: url(images/background/bg3.png);
+            background-size: cover;
+          ">
+        <div class="container">
+          <div class="row">
+            <!-- <div class="col-md-3 col-5 col-xl-2 col-lg-2 col-sm-6 footer-col-4">
+              <div class="widget widget_services border-0">
+                <h5 class="footer-title text-white">Company</h5>
+                <ul>
+                  <li><a href="./index.php">Home</a></li>
+                  <li><a href="./about.php">About</a></li>
+                  <li><a href="./gallery.php">Gallery</a></li>
+
+                  <li><a href="./contact.php">Contact </a></li>
+                </ul>
+              </div>
+            </div> -->
+            <!-- <div class="col-md-3 col-7 col-xl-2 col-lg-3 col-sm-6 footer-col-4">
+              <div class="widget widget_services border-0">
+                <h5 class="footer-title text-white">Useful Link</h5>
+                <ul>
+                  <li><a href="./domestic.php">Domestic LPG </a></li>
+                  <li><a href="./commercial.php">Commercial LPG</a></li>
+                  <li><a href="./safety-tips.php">Safety Tips</a></li>
+                  <li><a href="./franchisee.php">Franchisee</a></li>
+
+
+                </ul>
+              </div>
+            </div> -->
+            <div class="col-md-6 col-xl-4 col-lg-3 col-sm-6 footer-col-4">
+              <div class="widget widget_getintuch">
+                <h5 class="footer-title text-white">Contact us</h5>
+                <ul>
+                  <li>
+                    <i class="ti-location-pin"></i><strong>address</strong>Chamber Of Pharma Entrepreneurs,
+                    Chembaka Housing Colony, Olive Building
+                    Chalappuram,
+                    Calicut,Kerala
+                  </li>
+                  <li>
+                    <i class="ti-mobile"></i><strong>phone</strong>+91 9544101177
+
+                  </li>
+                  <li>
+                    <i class="ti-email"></i><strong>email</strong>copekerala@gmail.com
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-12 col-xl-4 col-lg-4 footer-col-4">
+              <div class="widget">
+                <h5 class="footer-title text-white">
+                Chamber Of Pharma
+                </h5>
+                <p class="text-capitalize m-b20">
+                  Malabar Fuel Corporation has been continuously involved in
+                  bottling of LPG as domestic and commercial under the brand
+                  name of MALABAR GAS since 1996.
+                </p>
+
+                <ul class="list-inline m-a0">
+                  <li>
+                    <a href="#" class="site-button facebook circle"><i class="fab fa-facebook-f"></i></a>
+                  </li>
+
+                  </li>
+                  <li>
+                    <a href="#" class="site-button instagram circle"><i class="fab fa-instagram"></i></a>
+                  </li>
+                  <li>
+                    <a href="#" class="site-button twitter circle"><i class="fab fa-twitter"></i></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="footer-bottom">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 col-sm-6 text-left">
+              <span> Designed & Developed by
+                <a href="https://www.codeorganization.com/" target="_blank">CodeOrganization &#128293;</a></span>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </footer>
